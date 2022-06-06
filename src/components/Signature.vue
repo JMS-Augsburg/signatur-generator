159f1731c6c7e5712fd30b76a19dfec834cd8ab3
Mikel Muennekhoff <mikel.muennekhoff@bdsu.de>
2022-06-06 17:17:06 +0200

    Strip demo, add demo component w/ props

---
+++ src/components/Signature.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    props: {
+        name: String,
+        email: String,
+        cell: String,
+        position: String
+    }
+}
+</script>
+
+<template>
+
+    <p>{{ name }} | {{ email }} | {{ cell }} | {{ position }} </p>
+
+</template>
