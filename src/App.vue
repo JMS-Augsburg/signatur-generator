22299377c7bf857f7f4871b1fdc3e0ddfbfef568
Mikel Muennekhoff <>
2022-07-03 14:58:45 +0200

    Beautify, position as dropdown

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ export default {
         name: `${this.profile.givenName} ${this.profile.surname}`,
         email: this.profile.mail,
         cell: this.profile.mobilePhone,
-        position: this.profile.jobTitle,
+        position: this.profile.jobTitle = "", // init as empty string to pre-select disable select option
       }
     }
   },
@@ -27,9 +27,20 @@ export default {
 
 <template>
 
-  <main style="padding: 4rem;">
+  <nav class="navbar">
+    <span>BDSU Signatur-Generator</span>
+    <button class="edit-profile"><a href="https://bdsuev-my.sharepoint.com/PersonImmersive.aspx" target="_blank">Profil in MS365 bearbeiten</a></button>
+  </nav>
+
+  <main>
+
+    <p class="info">
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+    </p>
+
     <Form @change="(e) => formData = e" :privateFormData="formData"/>
-    <button type="button" name="button" @click="copy">Copy</button>
+
+    <button class="copy-content" type="button" name="button" @click="copy">Signatur kopieren</button>
     <signature-preview
       ref="preview"
       :name="formData.name"
@@ -41,6 +52,66 @@ export default {
 </template>
 
 <style>
+
+nav.navbar {
+  width: 100%;
+  padding: 1rem 4rem;
+  background-color: rgb(132, 185, 63);
+  display: flex;
+  justify-content: space-between;
+}
+
+nav.navbar span {
+  font-size: 24pt;
+  font-weight: 600;
+  color: white;
+}
+
+button.edit-profile {
+  border: 0;
+  border-radius: 10px;
+  background-color: white;
+  padding: 0 1rem;
+  cursor: pointer;
+}
+
+button.edit-profile a {
+  color: rgb(132, 185, 63);
+  font-size: 14pt;
+  text-decoration: none;
+}
+
+button.edit-profile:hover {
+  background-color: #BBFFDD;
+}
+
+p.info {
+  color: #555;
+  margin-bottom: 1rem;
+}
+
+main {
+  padding: 1rem 4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+main > * {
+  margin: 1rem 0;
+}
+
+button.copy-content {
+  background-color: rgba(132, 185, 63, 0.5);
+  border: 0;
+  border-radius: 10px;
+  cursor: pointer;
+  padding: 10px auto;
+}
+
+button.copy-content:hover {
+  background-color: rgba(132, 185, 63, 0.8)
+}
 
 body {
   min-height: 100vh;
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -14,20 +14,58 @@ export default {
 </script>
 
 <template>
-    <div>
-        <label>Name:</label>
-        <input v-model="privateFormData.name" @change="emitChange" />
-    </div>
-    <div>
-        <label>Email:</label>
-        <input v-model="privateFormData.email" @change="emitChange" />
-    </div>
-    <div>
-        <label>Cell:</label>
-        <input v-model="privateFormData.cell" @change="emitChange" />
-    </div>
-    <div>
-        <label>Position:</label>
-        <input v-model="privateFormData.position" @change="emitChange" />
-    </div>
+        <table>
+            <tr>
+                <td><label>Vollständiger Name</label></td>
+                <td><input v-model="privateFormData.name" @change="emitChange" /></td>
+            </tr>
+            <tr>
+                <td><label>E-Mail-Adresse (Verein)</label></td>
+                <td><input v-model="privateFormData.email" @change="emitChange" /></td>
+            </tr>
+            <tr>
+                <td><label>Telefonnummer</label></td>
+                <td><input v-model="privateFormData.cell" @change="emitChange" /></td>
+            </tr>
+            <tr>
+                <td><label>Position (Ressort/Stabsstelle/...)</label></td>
+                <!-- <td><input v-model="privateFormData.position" @change="emitChange" /></td> -->
+                <td>
+                    <select v-model="privateFormData.position" @change="emitChange">
+                        <option disabled value="">Bitte wählen</option>
+                        <option>Ressort Öffentlichkeitsarbeit</option>
+                        <option>Ressort Unternehmenskontakte</option>
+                        <option>Ressort Netzwerk</option>
+                        <option>Stabsstelle Eventmanagement</option>
+                        <option>Stabsstelle IT</option>
+                        <option>Ressort Qualitätsmanagement</option>
+                        <option>Ressort Finanzen & Recht</option>
+                    </select>
+                </td>
+            </tr>
+        </table>
 </template>
+
+<style scoped>
+
+div.form {
+    display: flex;
+    justify-content: space-around;
+}
+
+input, select {
+    min-width: 50vh;
+    border: 1px solid #AAA;
+    border-radius: 10px;
+    padding: 5px 15px;
+}
+
+select {
+    background-color: #BBFFDD;
+}
+
+tr>td:first-child {
+    padding: 0.5rem 5rem 0.5rem 0;
+}
+
+</style>
