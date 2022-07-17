0338033b6d95498f0ed7618d02aed6071e787716
Roman Anasal <roman.anasal@jms-augsburg.de>
2022-07-17 23:57:25 +0200

    Simplify HTML used in Signature.vue
    use <br> for simple line breaks and
    <p> for paragraphs with multiple lines

--- src/components/Signature.vue
+++ src/components/Signature.vue
@@ -16,7 +16,7 @@ export default {
         }
 
         p {
-            margin: 0;
+            margin-block: 1em;
             padding-inline-end: 1rem;
         }
 
@@ -48,29 +48,36 @@ export default {
 
 <template>
     <div ref="container" @click="select">
-        <!--
-        Signature delimiter following RFC 3673
-        https://datatracker.ietf.org/doc/html/rfc3676#section-4.3
-        -->
-        <p>--&nbsp;</p>
-
         <p class="name">
+            <!--
+            Signature delimiter following RFC 3673
+            https://datatracker.ietf.org/doc/html/rfc3676#section-4.3
+            -->
+            --&nbsp;<br>
+
             <strong>{{ name }}</strong>
         </p>
 
-        <p v-if="position">{{ position }}</p>
-        <p v-if="role">{{ role }}</p>
+        <p v-if="position || role">
+            <template v-if="position">{{ position }}</template>
+            <br v-if="position && role">
+            <template v-if="role">{{ role }}</template>
+            <br>
+        </p>
 
         <hr>
 
-        <p v-if="cell">
-            Mobil: <a :href="'tel:' + cell.replace(/\s/g, '')">{{ cell }}</a>
-        </p>
-        <p v-if="email">
-            E-Mail: <a :href="'mailto:' + email">{{ email }}</a>
-        </p>
-
         <p>
+            <template v-if="cell">
+                Mobil: <a :href="'tel:' + cell.replace(/\s/g, '')">{{ cell }}</a>
+                <br>
+            </template>
+
+            <template v-if="email">
+                E-Mail: <a :href="'mailto:' + email">{{ email }}</a>
+                <br>
+            </template>
+
             JMS Augsburg e. V. Studentische Unternehmensberatung
         </p>
     </div>
