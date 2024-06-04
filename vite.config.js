cc639c3d6536079d74b8ddfe65a6f5fa683b43e0
timm0e <11885527+timm0e@users.noreply.github.com>
2024-06-04 22:10:57 +0200

    :sparkles: Make names configurable via vereinsdaten.yaml

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ViteYaml from '@modyfi/vite-plugin-yaml';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
         },
       },
     }),
+    ViteYaml(),
   ],
   resolve: {
     alias: {
